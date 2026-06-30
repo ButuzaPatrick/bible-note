f1df7996f8d3baa35259c7161a952067ffa702e0
ButuzaPatrick <patrickbutuza@gmail.com>
2026-06-30 16:05:42 +0100

    database reformatted fully

--- bible-note/src-python/database.py
+++ bible-note/src-python/database.py
@@ -5,10 +5,10 @@ DATABASE_URL = "sqlite:///bible_note.db"
 engine = create_engine(DATABASE_URL)
 
 def create_db() -> None:
-    """Create all database tables if they do not already exist."""
+    """Create all database tables if they do not already exist"""
     SQLModel.metadata.create_all(engine)
 
 def get_session() -> Iterator[Session]:
-    """Yield a database session for request-scoped use."""
+    """Yield a database session for request-scoped use"""
     with Session(engine) as session:
         yield session

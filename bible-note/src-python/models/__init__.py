cbe9cb94fd4d522c50469cd7515d230ce330296b
ButuzaPatrick <patrickbutuza@gmail.com>
2026-06-30 23:46:47 +0100

    db migration

---
+++ bible-note/src-python/models/__init__.py
@@ -0,0 +1,7 @@
+# src-python/models/__init__.py
+from .highlight import Highlight
+from .layer import Layer
+from .note import Note
+from .portal import Portal
+from .verse import Verse
+# add more tables here
